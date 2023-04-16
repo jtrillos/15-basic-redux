ac229d7e381798b9fac9521797864b779a504edb
Jaime M Trillos <jaimemanueltrillos@gmail.com>
2023-04-16 17:20:12 +0200

    adding another reduction to the code

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,12 @@
 //import { createStore } from "redux";
 import { createSlice, configureStore } from "@reduxjs/toolkit";
 
-const initialState = { counter: 0, showCounter: true };
+const initialCounterState = { counter: 0, showCounter: true };
+const initialAuthState = { isAuthenticated: false };
 
 const counterSlice = createSlice({
-  name: 'counter',
-  initialState,
+  name: "counter",
+  initialState: initialCounterState,
   reducers: {
     increment(state) {
       state.counter++;
@@ -18,8 +19,21 @@ const counterSlice = createSlice({
     },
     toggleCounter(state) {
       state.showCounter = !state.showCounter;
-    }
-  }
+    },
+  },
+});
+
+const authSlice = createSlice({
+  name: "authentication",
+  initialState: initialAuthState,
+  reducers: {
+    login(state) {
+      state.isAuthenticated = true;
+    },
+    logout(state) {
+      state.isAuthenticated = false;
+    },
+  },
 });
 
 /*const counterReducer = (state = initialState, action) => {
@@ -52,9 +66,10 @@ const counterSlice = createSlice({
 const store = createStore(counterReducer);*/
 
 const store = configureStore({
-  reducer: counterSlice.reducer
+  reducer: { counter: counterSlice.reducer, auth: authSlice.reducer },
 });
 
 export const counterActions = counterSlice.actions;
+export const authActions = authSlice.actions;
 
 export default store;
