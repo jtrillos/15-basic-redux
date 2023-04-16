45e80b55419c2070c4aef38ba5302be58e264c2e
Jaime M Trillos <jaimemanueltrillos@gmail.com>
2023-04-16 15:03:40 +0200

    base class components

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,16 +1,18 @@
-import { useSelector, useDispatch } from 'react-redux';
-import classes from './Counter.module.css';
+//import { useSelector, useDispatch } from "react-redux";
+import {connect} from "react-redux";
+import classes from "./Counter.module.css";
+import { Component } from "react";
 
-const Counter = () => {
+/*const Counter = () => {
   const dispatch = useDispatch();
-  const counter = useSelector(state => state.counter);
+  const counter = useSelector((state) => state.counter);
 
   const incrementHandler = () => {
-    dispatch({type: 'increment'});
+    dispatch({ type: "increment" });
   };
 
   const decrementHandler = () => {
-    dispatch({type: 'decrement'});
+    dispatch({ type: "decrement" });
   };
 
   const toggleCounterHandler = () => {};
@@ -26,6 +28,42 @@ const Counter = () => {
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
+};*/
+
+class Counter extends Component {
+  incrementHandler() {
+    this.props.increment();
+  }
+  decrementHandler() {
+    this.props.decrement();
+  }
+  toggleCounterHandler() {}
+  render() {
+    return (
+      <main className={classes.counter}>
+        <h1>Redux Counter</h1>
+        <div className={classes.value}>{this.props.counter}</div>
+        <div>
+          <button onClick={this.incrementHandler.bind(this)}>Increment</button>
+          <button onClick={this.decrementHandler.bind(this)}>Decrement</button>
+        </div>
+        <button onClick={this.toggleCounterHandler}>Toggle Counter</button>
+      </main>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    counter: state.counter
+  };
 };
 
-export default Counter;
+const mapDispatchToProps = dispatch => {
+  return {
+    increment: () => dispatch({type: "increment"}),
+    decrement: () => dispatch({type: "decrement"})
+  }
+};
+//export default Counter;
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
